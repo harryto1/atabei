2539e64968959abf74d161ff54c8516212b95445
harryto1 <harry.ruiz6@upr.edu>
2025-07-01 23:54:52 -0400

    feat: Revamp drawer widget with enhanced user profile display and navigation options

--- lib/components/drawer/drawer_widget.dart
+++ lib/components/drawer/drawer_widget.dart
@@ -1,4 +1,5 @@
 import 'package:atabei/features/auth/presentation/bloc/auth/auth_bloc.dart';
+import 'package:atabei/features/auth/presentation/bloc/auth/auth_event.dart';
 import 'package:atabei/features/auth/presentation/bloc/auth/auth_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,24 +9,125 @@ Drawer drawerWidget(BuildContext context) {
         child: BlocBuilder<AuthBloc, AuthState>(
           builder: (context, state) {
             if (state is AuthAuthenticated) {
-              return ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: state.user.pathToProfilePicture != null
-                      ? NetworkImage(state.user.pathToProfilePicture!)
-                      : null,
-                  child: state.user.pathToProfilePicture == null
-                      ? Text(state.user.displayName.substring(0, 1).toUpperCase())
-                      : null,
+              return Container(
+                padding: EdgeInsets.only(top: 20), 
+                alignment: Alignment.centerLeft,
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      alignment: Alignment.centerLeft,
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: state.user.pathToProfilePicture != null
+                              ? NetworkImage(state.user.pathToProfilePicture!)
+                              : null,
+                          radius: 30,
+                          child: state.user.pathToProfilePicture == null
+                              ? Text(state.user.displayName.substring(0, 1).toUpperCase(), style: Theme.of(context).textTheme.titleLarge,)
+                              : null,
+                        ),
+                        title: Text(state.user.displayName, style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600)),
+                        subtitle: Text(state.user.email),
+                      ),
+                    ),
+                    Divider(
+                      height: 1,
+                      thickness: 1,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      child: ListTile(
+                        minTileHeight: 75,
+                        leading: Icon(Icons.person),
+                        title: Text(
+                          'Profile',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.pushNamed(context, '/profile');
+                        },
+                      ),
+                    ),
+                    Divider(
+                      height: 1,
+                      thickness: 1,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      child: ListTile(
+                        minTileHeight: 75,
+                        leading: Icon(Icons.bookmark),
+                        title: Text(
+                          'Bookmarks',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.pushNamed(context, '/bookmarks');
+                        },
+                      ),
+                    ),
+                    Divider(
+                      height: 1,
+                      thickness: 1,
+                    ), 
+                    Container(
+                      alignment: Alignment.center,
+                      child: ListTile(
+                        minTileHeight: 75,
+                        leading: Icon(Icons.settings),
+                        title: Text(
+                          'Settings',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.pushNamed(context, '/settings');
+                        },
+                      ),
+                    ),
+                    Divider(
+                      height: 1,
+                      thickness: 1,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      child: ListTile(
+                        minTileHeight: 75,
+                        leading: Icon(Icons.logout, color: Colors.redAccent),
+                        title: Text(
+                          'Sign Out',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        onTap: () {
+                          context.read<AuthBloc>().add(AuthSignOutRequested());
+                          Navigator.pop(context); // Close the drawer after signing out
+                        },
+                      ),
+                    ),
+                  ],
                 ),
-                title: Text(state.user.displayName),
-                subtitle: Text(state.user.email),
               );
             } else {
-              return ListTile(
-                title: const Text('Login to see your profile'),
-                onTap: () {
-                  Navigator.pushNamed(context, '/login');
-                },
+              return Center(
+                child: ListTile(
+                  leading: Icon(Icons.login),
+                  title: Text('Login to see your profile', style: Theme.of(context).textTheme.titleMedium),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/login');
+                  },
+                ),
               );
             }
           },
